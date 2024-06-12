f4129e460920a980abf7be8134e04323c7e21655
devPadua <igortapadua@gmail.com>
2024-06-12 16:24:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let bpm = 0
 let batidas = []
 let ultimaBatida = 0
 let quintaBatida = 0
+let cliques = 5
 
 document.documentElement.addEventListener('click', function(){
     clique()
@@ -12,16 +13,19 @@ document.addEventListener('keydown', function(){
 })
 
 function clique(){
+    cliques--
     batidas.push(Date.now())
-    if (batidas.length >= 5){
+    if (batidas.length < 5){
+        bpm = `Aguardando dados, meça mais ${cliques} batimentos`
+    } else if (batidas.length >= 5 && batidas.length < 10){
         ultimaBatida = batidas[batidas.length-1]
         quintaBatida = batidas[batidas.length-4]
-        bpm = 60/((ultimaBatida - quintaBatida)/1000)*3
+        bpm = parseInt(60/((ultimaBatida - quintaBatida)/1000)*3)
     }
     resultado(bpm)
 }
 
 function resultado(bpm){
     let bpmHTML = document.getElementById('bpm')
-    bpmHTML.innerHTML = `BPM: ${parseInt(bpm)}`
+    bpmHTML.innerHTML = `BPM: ${bpm}`
 }
